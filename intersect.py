d5e420b5d07ae3612072c423bf6a7437935618a5
Joseph Mania <60258622+maniamartial@users.noreply.github.com>
2023-01-17 22:58:21 +0300

    Update intersect.py

--- intersect.py
+++ intersect.py
@@ -49,3 +49,40 @@ class Solution(object):
         :rtype: List[int]
         """
         
+        
+        #less memory
+        class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        re=[]
+        if len(nums1) <= len(nums2):
+            arr1 = nums1
+            arr2 = nums2
+        else:
+            arr1 = nums2
+            arr2 = nums1
+        for i in arr1:
+            if i in arr2:
+                arr2.pop(arr2.index(i))
+                re+=[i]
+        return re
+        
+        
+        #less time
+ class Solution(object):
+    def intersect(self, nums1, nums2):
+        d = {}
+        for num in nums2:
+            d[num] = 1 + d.get(num, 0)
+        ans = []
+        for num in nums1:
+            if num in d:
+                if d[num] > 0:
+                    ans.append(num)
+                    d[num] -= 1
+        return ans
+        
