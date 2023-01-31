ad86f77818b6e055dfb9a2567b3528c13a29f1c7
Joseph Mania <60258622+maniamartial@users.noreply.github.com>
2023-01-31 18:32:33 +0300

    Update reverseLinkedList.py

--- reverseLinkedList.py
+++ reverseLinkedList.py
@@ -8,6 +8,9 @@ Given the head of a singly linked list, reverse the list, and return the reverse
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+
+#run with 18ms
 class Solution(object):
     def reverseList(self, head):
         prev=None
@@ -19,3 +22,46 @@ class Solution(object):
             curr=next_node
         return prev
  
+
+
+#best time complexity run with 8ms
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        #return head
+
+        if(head is None): #empty linked list
+            return
+    
+        cur=head
+
+        while(cur.next):
+
+            node=cur.next  
+            cur.next=cur.next.next #point to the one after the next
+
+            node.next=head
+            head=node
+            
+       return head
+
+#best space complexity
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        t = None
+        c = head
+        p = None
+        while c:
+            t = c.next
+            c.next = p
+            p = c
+            c = t
+        return p
+             
